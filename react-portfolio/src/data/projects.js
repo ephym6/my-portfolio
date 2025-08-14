3c3190c3b7d0d525102e2cd65b9dbcf18122894a
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-08-14 18:40:26 +0300

    Added my projects

--- react-portfolio/src/data/projects.js
+++ react-portfolio/src/data/projects.js
@@ -1,4 +1,5 @@
 // My projects
+import {SiJavascript, SiSpringboot, SiHtml5, SiCss3, SiReact, SiVite, SiTailwindcss, SiPython} from "react-icons/si";
 
 export const projects = [
     {
@@ -8,5 +9,54 @@ export const projects = [
         image: "https://via.placeholder.com/300",
         link: "https://myportfolio.com",
         github: ""
+    },
+    {
+        title: "Hostel Booking System",
+        description: "A modern and robust hostel booking and management system built with Spring Boot for students",
+        tech: [
+            { icon: SiSpringboot, name: "Spring Boot" },
+            { icon: SiJavascript, name: "JavaScript" },
+            { icon: SiHtml5, name: "HTML5" },
+            { icon: SiCss3, name: "CSS3" }
+        ],
+        image: "https://via.placeholder.com/300",
+        link: "N/A",
+        github: "https://github.com/ephym6/hostel-booking-system.git"
+    },
+    {
+        title: "Vibe Culture Nation (E-commerce Website)",
+        description: "An e-commerce website for Vibe Culture Nation, featuring a modern design and user-friendly interface, for showing events, products, and more.",
+        tech: [
+            { icon: SiReact, name: "React" },
+            { icon: SiVite, name: "Vite" },
+            { icon: SiTailwindcss, name: "Tailwind CSS" },
+            { icon: "", name: "HeroUI" }
+        ],
+        image: "https://via.placeholder.com/300",
+        link: "https://vibe-culture-tau.vercel.app/",
+        github: ""
+    },
+    {
+        title: "Twitter Bot",
+        description: "A Twitter bot that automates posting and interacting with tweets based on specific keywords",
+        tech: [
+            { icon: SiPython, name: "Python", libraries: ["tweepy", "tkinter", "dotenv", "os"] }
+        ],
+        image: "https://via.placeholder.com/300",
+        link: "",
+        github: "https://github.com/ephym6/twitter-bot.git"
+    },
+    {
+        title: "Fantastic Four",
+        description: "A dynamic fan-made web app created in anticipation of Marvel Studios' The Fantastic Four: First Steps ",
+        tech: [
+            { icon: SiReact, name: "React" },
+            { icon: SiVite, name: "Vite" },
+            { icon: SiTailwindcss, name: "Tailwind CSS" },
+            { icon: SiHtml5, name: "HTML5", libraries: ["html5 audio api"] },
+        ],
+        image: "https://via.placeholder.com/300",
+        link: "https://fantastic-four.vercel.app/",
+        github: "https://github.com/ephym6/fantastic-four.git"
     }
 ];
