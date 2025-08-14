bde0d9bcb84929a684d06fd7c78990dd14368f76
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-08-14 17:04:21 +0300

    App background

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import './index.css'
 
 function App() {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-navy via-blue to-cyan text-paleBlue"></div>
+    )
 }
 
 export default App
